d2ab6243a0bf7e9c356784f2a6119a93e90e8e96
Omniscient <17525998+omnisci3nce@users.noreply.github.com>
2024-01-16 01:49:31 +1100

    fix package name change

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -3,7 +3,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use eframe::egui;
-use Zena::client::app::ZenaApp;
+use zena::client::app::ZenaApp;
 
 fn main() -> Result<(), eframe::Error> {
     pretty_env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
